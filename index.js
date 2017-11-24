28c4eae35e1295a0d2bc7a5d122afadd8d82dd2c
ArturGr <graefenstein@petesso.de>
2017-11-24 19:25:58 +0100

    Update index.js
    fix

--- index.js
+++ index.js
@@ -1,20 +1,72 @@
-'use strict';
+/*'use strict';
 import { 
   NativeModules, 
   requireNativeComponent, 
   View 
 } from 'react-native';
-import { PropTypes } from 'react';
+import PropTypes from 'prop-types';*/
+var React = require('react');
+import { Component, PropTypes } from 'react';  
+import { NativeModules,findNodeHandle, requireNativeComponent, View, Text, UIManager, DeviceEventEmitter } from 'react-native';
 
-var iface = {  
+/*var iface = {  
     name: 'MyEditText',
-    PropTypes: {
-        customval: PropTypes.string,
-		blured: PropTypes.boolean,
+    propTypes: {
+		blured: PropTypes.bool,
 		text:PropTypes.string,
-        ...View.propTypes // include the default view properties
+        ...View.propTypes, // include the default view properties
     }
+}*/
+
+
+class MyEditTextComponent extends Component {
+
+    constructor(props) {
+        super(props);
+        this._onChange = this._onChange.bind(this);
+    }
+
+    _onChange(event) {
+        if(!this.props.onDateChange) {
+            return;
+        }
+        this.props.onDateChange(event.nativeEvent);
+    }
+
+    render() {
+        return <MyEditText {...this.props} onChange={this._onChange} />;
+    }
+	
+	requestFocus() {
+		UIManager.dispatchViewManagerCommand(
+			findNodeHandle(this),
+			UIManager.MyEditText.Commands.requestFocus,
+			[],
+		);
+	}
+	
+	requestBlur() {
+		UIManager.dispatchViewManagerCommand(
+			findNodeHandle(this),
+			UIManager.MyEditText.Commands.requestBlur,
+			[],
+		);
+	}
 }
 
-var MyEditText = requireNativeComponent('MyEditText', iface);
-export default MyEditText;
+MyEditTextComponent.propTypes = {  
+    blured: PropTypes.bool,
+	text:PropTypes.string,
+    onDateChange: PropTypes.func,
+    ...View.propTypes,
+}
+
+const MyEditText = requireNativeComponent(`MyEditText`, MyEditTextComponent, {  
+    nativeOnly: { 
+        onChange: true,
+    },
+});
+
+
+//var MyEditText = requireNativeComponent('MyEditText', iface);
+export default MyEditTextComponent;
